b67a3dd04436e4a5f00fd1cab36d5dd5a897a0ce
demmerzel <137822189+demmerzel@users.noreply.github.com>
2023-06-28 20:06:40 +0200

    Add files via upload

---
+++ const.js
@@ -0,0 +1,118 @@
+var testTemplatePortfolio = 
+'<div class="cajaPortfolio" >'+
+    '<div class="imagenPortfolio" style="background-image: url({img})"></div>' +
+'</div>';
+var testTemplate = 
+'<div class="cajaLibro" >'+
+    '<div class="imagen" style="background-image: url({img})"></div>' +
+    '<div class="descripcion" style="grid-column:1; grid-row:2; text-align:center; ">{descripcion}</div>'+
+'</div>';
+
+
+
+var SUBMENU_BUTTONS = 
+'<div class="dropdown">'+
+    '<div class="submenuButton"  id="buttonPorftolio">Portfolio</div>'+
+    '<div class="dropdown-content" >'+
+    '    <a href="https://blog.hubspot.com/">Submenu1</a>'+
+    '    <a href="https://academy.hubspot.com/">Submenu2</a>'+
+    '    <a href="https://www.youtube.com/user/hubspot">Submenu3</a>'+
+    '</div>'+
+'</div>'+
+'<div class="dropdown" >'+
+    '<div class="submenuButton" id="buttonLibros">Libros</div>'+
+    '<div class="dropdown-content" >'+
+    '    <a >Submenu1</a>'+
+    '    <a >Submenu2</a>'+
+    '    <a >Submenu3</a>'+
+    '</div>'+
+'</div>'+
+'<div class="dropdown" >'+
+    '<div class="submenuButton"  id="buttonSobremi">Sobre mí</div>'+
+    '<div class="dropdown-content" >'+
+    '    <a >Submenu1</a>'+
+    '    <a >Submenu2</a>'+
+    '    <a >Submenu3</a>'+
+    '</div>'+
+'</div>'+
+'<div class="dropdown" >'+
+    '<div class="submenuButton"  id="buttonContacto">Contacto</div>'+
+    '<div class="dropdown-content" >'+
+    '    <a >Submenu1</a>'+
+    '    <a >Submenu2</a>'+
+    '    <a >Submenu3</a>'+
+    '</div>'+
+'</div>'+
+'<div class="dropdown" >'+
+    '<div class="submenuButton"  id="buttonRecomendaciones">Recomendaciones</div>'+
+    '<div class="dropdown-content" >'+
+    '    <a >Submenu1</a>'+
+    '    <a >Submenu2</a>'+
+    '    <a >Submenu3</a>'+
+    '</div>'+
+// '</div>'+
+'</div>';
+// '<div class="dropdown" >'+
+//     '<div class="redes" >'+
+//     '    <a class="red1"></a>'+ //instagram
+//     '    <a class="red2"></a>'+ //facebook
+//     '    <a class="red3"></a>'+ //twitter
+//     '    <a class="red4"></a>'+ //youtube
+//     '</div>'+
+// '</div>';
+
+var PARTNERS = 
+// '<div class="gridFlex">'+
+// '</div>';
+'<div class="gridFlex">'+
+'<a class="partnerAmazon" href="https://www.amazon.com"></a>'+
+'<a class="partnerFnac" href="https://www.fnac.com"></a>'+
+'<a class="partnerLacasadellibro" href="https://www.lacasadellibro.com"></a>'+
+'</div>';
+
+
+var REDES = 
+    '<div class="gridFlex" >'+
+    '    <a class="red1" href="https://www.instagram.com"></a>'+ //instagram
+    '    <a class="red2" href="https://www.facebook.com"></a>'+ //facebook
+    '    <a class="red3" href="https://www.twitter.com"></a>'+ //twitter
+    '    <a class="red4" href="https://www.linkedin.com"></a>'+ //youtube
+    '</div>';
+
+// var CONTACTO = 
+// <h1>h1 h1</h1>
+// <a>a de a</a>
+// <p>parrafo</p>
+// <input type="text" id="fname" name="fname"></input>
+
+var BODY_TEXT = 
+
+ "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut gravida hendrerit porttitor. Aliquam erat volutpat. Donec tempus pellentesque odio, et consectetur ex tristique eget. Interdum et malesuada fames ac ante ipsum primis in faucibus. In hac habitasse platea dictumst. Aenean tincidunt ex sit amet ornare aliquam. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vivamus vel molestie est. Proin arcu neque, tincidunt quis nunc id, consequat volutpat justo. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Proin vel auctor eros, non volutpat felis.<br/>"+
+ "<br/> "+
+ "Suspendisse vehicula, nisi vitae ultrices mollis, lectus sapien blandit mi, et tincidunt est turpis id metus. Donec iaculis augue pulvinar ligula tempor malesuada. Suspendisse potenti. Donec molestie, est ac mollis varius, magna ante malesuada dolor, pretium accumsan eros ante quis orci. Morbi id pulvinar libero, venenatis posuere risus. Donec laoreet et justo vel posuere. Cras sem lacus, suscipit et posuere vitae, tincidunt at lacus. Donec tempor aliquam eros. Quisque sed tellus felis. Sed rutrum leo dui, et aliquam lacus interdum sit amet. Sed quis ex faucibus, faucibus dui rutrum, lobortis nisi. Proin eget dignissim nisl, sit amet tempor metus. Praesent varius id leo quis egestas. Vivamus magna lacus, egestas blandit malesuada sed, euismod in nulla. Pellentesque nec leo id odio iaculis facilisis ut nec nisl.<br/>"+
+ "<br/>"+
+ "Nullam interdum ut ipsum sit amet ultrices. Proin a lobortis augue. Aenean mattis mi in viverra feugiat. Suspendisse suscipit ipsum id libero vehicula, eu dignissim nisl feugiat. Curabitur imperdiet suscipit dolor, sed viverra lorem finibus in. Donec id leo in arcu iaculis maximus eu sed mi. Maecenas varius pellentesque urna, sit amet suscipit nulla.<br/>"+
+ "<br/>"+
+ "Nunc volutpat egestas lacus et euismod. Sed finibus lacus orci, porttitor bibendum sem sodales vitae. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam consequat ex quis nulla mattis, a iaculis nunc feugiat. Quisque nec tempus magna. Curabitur at lectus vel turpis commodo suscipit. Donec fermentum lectus in risus elementum, sit amet venenatis nisl finibus. Aenean elementum venenatis tempus. Suspendisse velit risus, volutpat in velit in, tristique pulvinar eros. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Donec ac mollis lectus. In ornare dolor dui, sed placerat libero bibendum eget. Etiam et placerat ex, eu pellentesque arcu.<br/>"+
+ "<br/>"+ "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut gravida hendrerit porttitor. Aliquam erat volutpat. Donec tempus pellentesque odio, et consectetur ex tristique eget. Interdum et malesuada fames ac ante ipsum primis in faucibus. In hac habitasse platea dictumst. Aenean tincidunt ex sit amet ornare aliquam. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vivamus vel molestie est. Proin arcu neque, tincidunt quis nunc id, consequat volutpat justo. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Proin vel auctor eros, non volutpat felis.<br/>"+
+ "<br/>"+
+ "Suspendisse vehicula, nisi vitae ultrices mollis, lectus sapien blandit mi, et tincidunt est turpis id metus. Donec iaculis augue pulvinar ligula tempor malesuada. Suspendisse potenti. Donec molestie, est ac mollis varius, magna ante malesuada dolor, pretium accumsan eros ante quis orci. Morbi id pulvinar libero, venenatis posuere risus. Donec laoreet et justo vel posuere. Cras sem lacus, suscipit et posuere vitae, tincidunt at lacus. Donec tempor aliquam eros. Quisque sed tellus felis. Sed rutrum leo dui, et aliquam lacus interdum sit amet. Sed quis ex faucibus, faucibus dui rutrum, lobortis nisi. Proin eget dignissim nisl, sit amet tempor metus. Praesent varius id leo quis egestas. Vivamus magna lacus, egestas blandit malesuada sed, euismod in nulla. Pellentesque nec leo id odio iaculis facilisis ut nec nisl.<br/>"+
+ "<br/>"+
+ "Nullam interdum ut ipsum sit amet ultrices. Proin a lobortis augue. Aenean mattis mi in viverra feugiat. Suspendisse suscipit ipsum id libero vehicula, eu dignissim nisl feugiat. Curabitur imperdiet suscipit dolor, sed viverra lorem finibus in. Donec id leo in arcu iaculis maximus eu sed mi. Maecenas varius pellentesque urna, sit amet suscipit nulla.<br/>"+
+ "<br/>"+
+ "Nunc volutpat egestas lacus et euismod. Sed finibus lacus orci, porttitor bibendum sem sodales vitae. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam consequat ex quis nulla mattis, a iaculis nunc feugiat. Quisque nec tempus magna. Curabitur at lectus vel turpis commodo suscipit. Donec fermentum lectus in risus elementum, sit amet venenatis nisl finibus. Aenean elementum venenatis tempus. Suspendisse velit risus, volutpat in velit in, tristique pulvinar eros. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Donec ac mollis lectus. In ornare dolor dui, sed placerat libero bibendum eget. Etiam et placerat ex, eu pellentesque arcu.<br/>"+
+ "<br/>"+ "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut gravida hendrerit porttitor. Aliquam erat volutpat. Donec tempus pellentesque odio, et consectetur ex tristique eget. Interdum et malesuada fames ac ante ipsum primis in faucibus. In hac habitasse platea dictumst. Aenean tincidunt ex sit amet ornare aliquam. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vivamus vel molestie est. Proin arcu neque, tincidunt quis nunc id, consequat volutpat justo. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Proin vel auctor eros, non volutpat felis.<br/>"+
+ "<br/>"+
+ "Suspendisse vehicula, nisi vitae ultrices mollis, lectus sapien blandit mi, et tincidunt est turpis id metus. Donec iaculis augue pulvinar ligula tempor malesuada. Suspendisse potenti. Donec molestie, est ac mollis varius, magna ante malesuada dolor, pretium accumsan eros ante quis orci. Morbi id pulvinar libero, venenatis posuere risus. Donec laoreet et justo vel posuere. Cras sem lacus, suscipit et posuere vitae, tincidunt at lacus. Donec tempor aliquam eros. Quisque sed tellus felis. Sed rutrum leo dui, et aliquam lacus interdum sit amet. Sed quis ex faucibus, faucibus dui rutrum, lobortis nisi. Proin eget dignissim nisl, sit amet tempor metus. Praesent varius id leo quis egestas. Vivamus magna lacus, egestas blandit malesuada sed, euismod in nulla. Pellentesque nec leo id odio iaculis facilisis ut nec nisl.<br/>"+
+ "<br/>"+
+ "Nullam interdum ut ipsum sit amet ultrices. Proin a lobortis augue. Aenean mattis mi in viverra feugiat. Suspendisse suscipit ipsum id libero vehicula, eu dignissim nisl feugiat. Curabitur imperdiet suscipit dolor, sed viverra lorem finibus in. Donec id leo in arcu iaculis maximus eu sed mi. Maecenas varius pellentesque urna, sit amet suscipit nulla.<br/>"+
+ "<br/>"+
+ "Nunc volutpat egestas lacus et euismod. Sed finibus lacus orci, porttitor bibendum sem sodales vitae. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam consequat ex quis nulla mattis, a iaculis nunc feugiat. Quisque nec tempus magna. Curabitur at lectus vel turpis commodo suscipit. Donec fermentum lectus in risus elementum, sit amet venenatis nisl finibus. Aenean elementum venenatis tempus. Suspendisse velit risus, volutpat in velit in, tristique pulvinar eros. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Donec ac mollis lectus. In ornare dolor dui, sed placerat libero bibendum eget. Etiam et placerat ex, eu pellentesque arcu.<br/>"+
+ "<br/>"+ "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut gravida hendrerit porttitor. Aliquam erat volutpat. Donec tempus pellentesque odio, et consectetur ex tristique eget. Interdum et malesuada fames ac ante ipsum primis in faucibus. In hac habitasse platea dictumst. Aenean tincidunt ex sit amet ornare aliquam. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vivamus vel molestie est. Proin arcu neque, tincidunt quis nunc id, consequat volutpat justo. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Proin vel auctor eros, non volutpat felis.<br/>"+
+ "<br/>"+
+ "Suspendisse vehicula, nisi vitae ultrices mollis, lectus sapien blandit mi, et tincidunt est turpis id metus. Donec iaculis augue pulvinar ligula tempor malesuada. Suspendisse potenti. Donec molestie, est ac mollis varius, magna ante malesuada dolor, pretium accumsan eros ante quis orci. Morbi id pulvinar libero, venenatis posuere risus. Donec laoreet et justo vel posuere. Cras sem lacus, suscipit et posuere vitae, tincidunt at lacus. Donec tempor aliquam eros. Quisque sed tellus felis. Sed rutrum leo dui, et aliquam lacus interdum sit amet. Sed quis ex faucibus, faucibus dui rutrum, lobortis nisi. Proin eget dignissim nisl, sit amet tempor metus. Praesent varius id leo quis egestas. Vivamus magna lacus, egestas blandit malesuada sed, euismod in nulla. Pellentesque nec leo id odio iaculis facilisis ut nec nisl.<br/>"+
+ "<br/>"+
+ "Nullam interdum ut ipsum sit amet ultrices. Proin a lobortis augue. Aenean mattis mi in viverra feugiat. Suspendisse suscipit ipsum id libero vehicula, eu dignissim nisl feugiat. Curabitur imperdiet suscipit dolor, sed viverra lorem finibus in. Donec id leo in arcu iaculis maximus eu sed mi. Maecenas varius pellentesque urna, sit amet suscipit nulla.<br/>"+
+ "<br/>"+
+ "Nunc volutpat egestas lacus et euismod. Sed finibus lacus orci, porttitor bibendum sem sodales vitae. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam consequat ex quis nulla mattis, a iaculis nunc feugiat. Quisque nec tempus magna. Curabitur at lectus vel turpis commodo suscipit. Donec fermentum lectus in risus elementum, sit amet venenatis nisl finibus. Aenean elementum venenatis tempus. Suspendisse velit risus, volutpat in velit in, tristique pulvinar eros. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Donec ac mollis lectus. In ornare dolor dui, sed placerat libero bibendum eget. Etiam et placerat ex, eu pellentesque arcu.<br/>"+
+ "<br/>";
---
+++ main.js
@@ -0,0 +1,379 @@
+var m_menuSelected = null;
+
+
+function asignarEvento(){
+
+}
+
+
+function fn_reemplazarContenido(id, html){
+    document.addEventListener('DOMContentLoaded', function () {
+        var a = document.getElementById(id);
+        a.innerHTML = html;
+    }, false);
+}
+
+function fn_setPortfolio() {
+    var html = '<div class="grid gridColumns" style="">';
+    for(var i in portfolio){
+        var item = portfolio[i];
+        html += testTemplatePortfolio.replace("{img}", item.img);
+    }
+    html += "<div>";
+
+    fn_reemplazarContenido("portfolio", html);
+}
+
+function fn_setLibros() {
+    var html = '<div class="cabeceraLibros" style=""></div>';
+    html += '<div class="gridLibros libros" style="">';
+    for(var i in  libros){
+        var item = libros[i];
+        html += testTemplate.replace("{img}", item.img).replace("{descripcion}", item.subtitulo);
+    }
+    html += "<div>";
+
+    fn_reemplazarContenido("libros", html);
+}
+
+function fn_setMenu() {
+    document.addEventListener('DOMContentLoaded', function () {
+        var a = document.getElementById("menu");
+        // a.innerHTML = BODY_TEXT;
+    }, false);
+}
+
+function fn_hideCapa(id){
+    var capa = document.getElementById(id);
+    capa.style.display = "none";
+}
+function fn_showCapa(id){
+    var capa = document.getElementById(id);
+    capa.style.display = "block";
+}
+function fn_setCapas(){
+    fn_hideCapa("portfolio")
+    fn_hideCapa("libros")
+    fn_hideCapa("sobremi")
+    fn_hideCapa("contacto")
+    fn_hideCapa("recomendaciones")
+    fn_showCapa(m_menuSelected);
+}
+function fn_setSubmenuButtons() {
+    document.addEventListener('DOMContentLoaded', function () {
+        var a = document.getElementById("submenuButtons");
+        a.innerHTML = SUBMENU_BUTTONS;
+
+
+        var b1 = document.getElementById("buttonPorftolio");
+        b1.addEventListener("click", function(){
+            m_menuSelected = "portfolio"
+            fn_setCapas();
+        });
+        var b2 = document.getElementById("buttonLibros");
+        b2.addEventListener("click", function(){
+            m_menuSelected = "libros"
+            fn_setCapas();
+        });
+        var b3 = document.getElementById("buttonSobremi");
+        b3.addEventListener("click", function(){
+            m_menuSelected = "sobremi"
+            fn_setCapas();
+        });
+        var b4 = document.getElementById("buttonContacto");
+        b4.addEventListener("click", function(){
+            m_menuSelected = "contacto"
+            fn_setCapas();
+        });
+        var b5 = document.getElementById("buttonRecomendaciones");
+        b5.addEventListener("click", function(){
+            m_menuSelected = "recomendaciones"
+            fn_setCapas();
+        });
+
+
+        fn_hideCapa("portfolio")
+        fn_hideCapa("libros")
+        fn_hideCapa("sobremi")
+        fn_hideCapa("contacto")
+        fn_hideCapa("recomendaciones")
+        m_menuSelected = "portfolio";
+        fn_showCapa(m_menuSelected);
+
+
+
+
+        buttonPorftolio
+        buttonLibros
+        buttonSobremi
+        buttonContacto
+        buttonRecomendaciones
+
+        // var sb1 = document.getElementById("submenuButton1");
+        // sb1.addEventListener('mouseover', function () {
+        //     // alert("hola");
+        // });
+        
+    }, false);
+}
+
+function fn_setSobremi(){
+
+    var html = 'Sobre Mi Sobre Mi Sobre Mi Sobre Mi Sobre Mi Sobre Mi Sobre Mi Sobre Mi Sobre Mi Sobre Mi Sobre Mi Sobre Mi Sobre Mi Sobre Mi Sobre Mi Sobre Mi Sobre Mi Sobre Mi Sobre Mi Sobre Mi Sobre Mi';
+    // fn_reemplazarContenido("biografia", html);
+}
+function fn_setContacto(){
+    // var html = CONTACTO;
+    // fn_reemplazarContenido("contacto", html);
+}
+function fn_setRecomendaciones(){
+    var html = 'Recomendaciones Recomendaciones Recomendaciones Recomendaciones Recomendaciones Recomendaciones Recomendaciones Recomendaciones Recomendaciones Recomendaciones Recomendaciones Recomendaciones Recomendaciones';
+    fn_reemplazarContenido("recomendaciones", html);
+}
+
+function fn_setPartners(){
+    var html = PARTNERS;
+    fn_reemplazarContenido("partners", html);
+}
+function fn_setRedes(){
+    var html = REDES;
+    fn_reemplazarContenido("redes", html);
+}
+function fn_getTxt(id){
+    var element = document.getElementById(id);
+    var text = element.value;
+    return text;
+}
+function fn_check(tipo){
+    var error = 0;
+    switch(tipo){
+        case "nombre":
+            var a = document.getElementById("nombreInput");
+            if(a.value == ""){
+                error = 1;
+            }
+
+            if(error){
+                var b = document.getElementById("nombre");
+                b.style.border = "1px solid red";
+                return false;
+            }
+            break;
+
+        case "apellidos":
+            var a = document.getElementById("apellidosInput");
+            if(a.value == ""){
+                var b = document.getElementById("apellidos");
+                b.style.border = "1px solid red";
+            }
+            return false;
+            break;
+
+        case "email":
+            var a = document.getElementById("emailInput");
+            if(a.value == ""){
+                var b = document.getElementById("email");
+                b.style.border = "1px solid red";
+            }
+            return false;
+            break;
+             
+        case "mensaje":
+            var a = document.getElementById("mensajeInput");
+            if(a.value == ""){
+                var b = document.getElementById("mensaje");
+                b.style.border = "1px solid red";
+            }
+            return false;
+            break;
+    }
+    return true;
+}
+function fn_setEnviar(){
+    var a = document.getElementById("enviar");
+    a.addEventListener("click", function(){
+        // alert("AA")
+        //Comprobar campos
+        var error = 0;
+        if(!fn_check("nombre")){
+            error = 1;
+        }
+        if(!fn_check("apellidos")){
+            error = 1;
+        }
+        if(!fn_check("email")){
+            error = 1;
+        }
+        if(!fn_check("mensaje")){
+            error = 1;
+        }
+
+        if(error){
+            return;
+        }
+    });
+}
+////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////
+fn_setMenu();
+fn_setPortfolio();
+fn_setLibros();
+fn_setSubmenuButtons();
+fn_setSobremi();
+fn_setContacto();
+fn_setRecomendaciones();
+fn_setPartners();
+fn_setRedes();
+document.addEventListener('DOMContentLoaded', function () {
+    fn_setEnviar();
+
+});
+
+//navigator.geolocation.getCurrentPosition
+const options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+};
+function success(pos) {
+    const crd = pos.coords;
+    console.log('Tu ubicación actual es:');
+    console.log(`Latitud : ${crd.latitude}`);
+    console.log(`Longitud: ${crd.longitude}`);
+    console.log(`Más o menos ${crd.accuracy} metros.`);
+}
+function error(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+}
+navigator.geolocation.getCurrentPosition(success, error, options);
+
+var idioma = navigator.language;
+console.log(idioma);
+
+var agenteDeUsuario = navigator.userAgent;
+console.log(agenteDeUsuario);
+
+navigator.platform
+navigator.plugins
+navigator.onLine
+screen.width
+screen.height
+window.location
+document.referrer
+localStorage
+sessionStorage
+
+// Devuelve el nombre del código interno de la familia de navegadores del usuario.
+var navigator_appCodeName = navigator.appCodeName;
+
+// Devuelve el nombre del navegador del usuario.
+var navigator_appName = navigator.appName;
+
+// Devuelve la versión del navegador del usuario.
+var navigator_appVersion = navigator.appVersion;
+
+// Devuelve true si el usuario tiene habilitadas las cookies en su navegador, false en caso contrario.
+var navigator_cookieEnabled = navigator.cookieEnabled;
+
+// Devuelve 1 si el usuario ha habilitado la opción "No realizar seguimiento" en su navegador, 0 si no lo ha hecho o null si no está definido.
+var navigator_doNotTrack = navigator.doNotTrack;
+
+// Devuelve un objeto que permite acceder a la ubicación del usuario, siempre y cuando este haya otorgado permiso.
+var navigator_geolocation = navigator.geolocation;
+
+// Devuelve el número de núcleos de CPU disponibles en el dispositivo del usuario.
+var navigator_hardwareConcurrency = navigator.hardwareConcurrency;
+
+// Devuelve el idioma preferido del usuario, en función de la configuración del navegador.
+var navigator_language = navigator.language;
+
+// Devuelve el número máximo de puntos de contacto táctil que pueden detectarse en el dispositivo del usuario.
+var navigator_maxTouchPoints = navigator.maxTouchPoints;
+
+// Devuelve un objeto que permite acceder a los dispositivos multimedia conectados al dispositivo del usuario, como la cámara y el micrófono.
+var navigator_mediaDevices = navigator.mediaDevices;
+
+// Devuelve una lista de los tipos MIME admitidos por el navegador del usuario.
+var navigator_mimeTypes = navigator.mimeTypes;
+
+// Devuelve true si el usuario está conectado a Internet y false si no lo está.
+var navigator_onLine = navigator.onLine;
+
+// Devuelve el sistema operativo en el que se está ejecutando el navegador del usuario.
+var navigator_oscpu = navigator.oscpu;
+
+// Devuelve un objeto que permite acceder a los permisos otorgados por el usuario para el acceso a dispositivos y características del navegador.
+var navigator_permissions = navigator.permissions;
+
+// Devuelve el sistema operativo en el que se está ejecutando el navegador del usuario.
+var navigator_platform = navigator.platform;
+
+// Devuelve una lista de los plugins instalados en el navegador del usuario.
+var navigator_plugins = navigator.plugins;
+
+// Devuelve el nombre del producto que está utilizando el usuario.
+var navigator_product = navigator.product;
+
+// Devuelve una cadena que contiene información sobre el agente de usuario del navegador, como el nombre y la versión del navegador.
+var navigator_userAgent = navigator.userAgent;
+
+// Devuelve el nombre del proveedor del navegador del usuario.
+var navigator_vendor = navigator.vendor;
+
+// Devuelve la versión del proveedor del navegador del usuario.
+var navigator_vendorSub = navigator.vendorSub;
+
+//  Devuelven el ancho y la altura de la pantalla del usuario.
+var screen_width = screen.width;
+var screen_height = screen.height;
+
+//  Devuelven el ancho y la altura de la pantalla del usuario, descontando la barra de herramientas y los bordes de la ventana del navegador.
+var screen_availWidth = screen.availWidth;
+var screen_availHeight = screen.availHeight;
+
+// Devuelve información sobre la URL actual, incluyendo la dirección, el protocolo y los parámetros de la URL.
+var window_location = window.location;
+
+// Devuelve la URL de la página anterior que llevó al usuario a la página actual.
+var document_referrer = document.referrer;
+
+// Estos objetos permiten almacenar datos localmente en el navegador del usuario.
+var localStorage = localStorage;
+var sessionStorage = sessionStorage;
+
+// Devuelve el ID de compilación del navegador del usuario.
+var navigator_buildID = navigator.buildID;
+
+// Devuelve información sobre la conexión de red del usuario.
+var navigator_connection = navigator.connection;
+
+// Devuelve un objeto que permite acceder a las credenciales guardadas en el navegador del usuario.
+var navigator_credentials = navigator.credentials;
+
+// Devuelve la cantidad de memoria disponible en el dispositivo del usuario.
+var navigator_deviceMemory = navigator.deviceMemory;
+
+// Devuelve un objeto que permite acceder a información sobre el teclado del usuario.
+var navigator_keyboard = navigator.keyboard;
+
+// Devuelve una lista de los idiomas preferidos del usuario.
+var navigator_languages = navigator.languages;
+
+// Devuelve información sobre las capacidades multimedia del navegador del usuario.
+var navigator_mediaCapabilities = navigator.mediaCapabilities;
+
+// Devuelve un objeto que permite controlar la sesión de reproducción multimedia del navegador del usuario.
+var navigator_mediaSession = navigator.mediaSession;
+
+
+// function downloadFile(url, fileName) {
+//     const anchorElement = document.createElement("a");
+//     anchorElement.href = url;
+//     anchorElement.download = fileName;
+//     anchorElement.click();
+//   }
+  
+//   // Ejemplo de uso: descarga un archivo llamado "ejemplo.pdf" desde la URL "https://ejemplo.com/ejemplo.pdf"
+//   downloadFile("https://ejemplo.com/ejemplo.pdf", "ejemplo.pdf");
+   
